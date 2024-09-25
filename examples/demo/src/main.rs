3305aa02d3a46f84b9ea60498a6a6ef1135f1978
ritzier <ritzierwheat@gmail.com>
2024-09-25 17:30:29 +0800

    feat:

--- examples/demo/src/main.rs
+++ examples/demo/src/main.rs
@@ -1,11 +1,14 @@
 mod action;
 mod app;
 mod config;
+mod errors;
 mod logging;
+mod tui;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     crate::logging::init()?;
 
+    println!("hi");
     Ok(())
 }
