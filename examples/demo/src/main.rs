55ade4e0ff5bc0b755debc3db5295a6115abc73d
ritzier <ritzierwheat@gmail.com>
2024-09-25 17:46:31 +0800

    feat(tui): add `tui.rs` & `Cli::parse()` to entry

--- examples/demo/src/main.rs
+++ examples/demo/src/main.rs
@@ -1,5 +1,9 @@
+use clap::Parser;
+use cli::Cli;
+
 mod action;
 mod app;
+mod cli;
 mod config;
 mod errors;
 mod logging;
@@ -9,6 +13,8 @@ mod tui;
 async fn main() -> color_eyre::Result<()> {
     crate::logging::init()?;
     crate::errors::init()?;
+
+    let _args = Cli::parse();
 
     println!("hi");
     Ok(())
